95a00eb67c70a0ad8d5fe1f41fe9bad694c01648
PhaniGaddipati <phanigaddipati@gmail.com>
2016-08-02 17:53:32 -0400

    use GifImageView and GifDrawable for image view activity

--- app/src/main/java/org/centum/techconnect/activities/ImageViewActivity.java
+++ app/src/main/java/org/centum/techconnect/activities/ImageViewActivity.java
@@ -2,7 +2,6 @@ package org.centum.techconnect.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
@@ -10,16 +9,19 @@ import com.squareup.picasso.Picasso;
 import org.centum.techconnect.R;
 
 import java.io.File;
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
 import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class ImageViewActivity extends AppCompatActivity {
 
     public static final String EXTRA_PATH = "path";
     @Bind(R.id.photoImageView)
-    ImageView imageView;
+    GifImageView imageView;
 
     private PhotoViewAttacher attacher;
 
@@ -33,19 +35,31 @@ public class ImageViewActivity extends AppCompatActivity {
 
         if (getIntent() != null && getIntent().getStringExtra(EXTRA_PATH) != null) {
             String path = getIntent().getStringExtra(EXTRA_PATH);
-            Picasso.with(this)
-                    .load(new File(path))
-                    .into(imageView, new Callback() {
-                        @Override
-                        public void onSuccess() {
-                            attacher.update();
-                        }
+            if (path.toLowerCase().endsWith(".gif")) {
+                try {
+                    GifDrawable drawable = new GifDrawable(path);
+                    imageView.setImageDrawable(drawable);
+                } catch (IOException e) {
+                    imageView.setImageResource(android.R.drawable.stat_notify_error);
+                    e.printStackTrace();
+                }
+                attacher.update();
+            } else {
+                Picasso.with(this)
+                        .load(new File(path))
+                        .into(imageView, new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                attacher.update();
+                            }
 
-                        @Override
-                        public void onError() {
-
-                        }
-                    });
+                            @Override
+                            public void onError() {
+                                imageView.setImageResource(android.R.drawable.stat_notify_error);
+                                attacher.update();
+                            }
+                        });
+            }
         }
     }
 }
