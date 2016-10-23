08f3cdab9d6522cd1ce08a10e078da7f741789af
PhaniGaddipati <phanigaddipati@gmail.com>
2016-10-23 11:14:28 -0400

    Utils class

---
+++ app/src/main/java/org/techconnect/Utils.java
@@ -0,0 +1,20 @@
+package org.techconnect;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Phani on 10/23/2016.
+ */
+
+public class Utils {
+
+    public static boolean isNetworkAvailable(Context c) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+}
