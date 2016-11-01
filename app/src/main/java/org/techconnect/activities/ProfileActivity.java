9c80316b27b818e0aabd78a8a94eded06457c996
dwtroldhaugen@gmail.com <Chelonia1>
2016-11-01 13:07:44 -0400

    Initial Edit Skills table, do not update  profile

--- app/src/main/java/org/techconnect/activities/ProfileActivity.java
+++ app/src/main/java/org/techconnect/activities/ProfileActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import org.centum.techconnect.R;
@@ -36,10 +38,20 @@ public class ProfileActivity extends AppCompatActivity {
         //Add organization, email, and skills to the list below
         final TextView org = (TextView) findViewById(R.id.profile_work);
         final TextView email = (TextView) findViewById(R.id.profile_email);
-        final TextView skills = (TextView) findViewById(R.id.profile_skills);
         org.setText(user.getOrganization());
         email.setText(user.getEmail());
-        skills.setText(TextUtils.join(",",user.getExpertises()));
+
+        //Create all rows from list of skills in profile
+        final TableLayout skills_table = (TableLayout) findViewById(R.id.skills_table);
+        for (String skill : user.getExpertises()) {
+            TableRow toAdd = (TableRow) getLayoutInflater().inflate(R.layout.skill_tablerow,null,false);
+            TextInputLayout addSkill = (TextInputLayout) toAdd.findViewById(R.id.edit_skill_layout);
+            addSkill.setVisibility(View.GONE);
+            TextView toAddText = (TextView)  toAdd.findViewById(R.id.skill_text);
+            toAddText.setText(skill);
+            toAddText.setVisibility(View.VISIBLE);
+            skills_table.addView(toAdd);
+        }
 
         //Register the edit text fields for changing account info
         final TextInputLayout edit_org_layout = (TextInputLayout) findViewById(R.id.edit_work_layout);
@@ -74,11 +86,37 @@ public class ProfileActivity extends AppCompatActivity {
                 isEditing = !isEditing;
             }
         });
-        ImageButton editSkill = (ImageButton) findViewById(R.id.edit_skill_button);
-        editSkill.setOnClickListener(new View.OnClickListener() {
+        final ImageButton addSkill = (ImageButton) findViewById(R.id.add_skill_button);
+        addSkill.setOnClickListener(new View.OnClickListener() {
+            boolean isAdding = false;
+            TableRow toAdd;
+            TextInputLayout inputLayout;
+            ImageView icon;
+            EditText add_skill;
+            TextView skill_text;
             @Override
             public void onClick(View view) {
-                //Edit Activity/Fragment
+                //Add a new row to the list of skills
+                if (!isAdding) {
+                    toAdd = (TableRow) getLayoutInflater().inflate(R.layout.skill_tablerow,null,false);
+                    inputLayout = (TextInputLayout) toAdd.findViewById(R.id.edit_skill_layout);
+                    icon = (ImageView) toAdd.findViewById(R.id.skill_icon);
+                    add_skill = (EditText) toAdd.findViewById(R.id.edit_skill_text);
+                    icon.setImageResource(R.drawable.ic_add_box_black_24dp);
+                    add_skill.setText("Add Skill");
+                    addSkill.setImageResource(R.drawable.ic_done_black_24dp);
+                    skills_table.addView(toAdd);
+                } else { //Stopping adding
+                    toAdd = (TableRow) skills_table.getChildAt(skills_table.getChildCount()- 1 );
+                    skill_text = (TextView) toAdd.findViewById(R.id.skill_text);
+                    skill_text.setText(add_skill.getText());
+
+                    icon.setImageResource(R.drawable.ic_build_black_24dp);
+                    skill_text.setVisibility(View.VISIBLE);
+                    inputLayout.setVisibility(View.GONE);
+                    addSkill.setImageResource(R.drawable.ic_add_box_black_24dp);
+                }
+                isAdding = !isAdding;
             }
         });
     }
