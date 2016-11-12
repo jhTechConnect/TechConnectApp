f53fd013aae0dadb56957064592afdd2946e2489
dwtroldhaugen@gmail.com <Chelonia1>
2016-11-12 17:01:15 -0500

    Removed Contact class

---
+++ app/src/main/java/org/techconnect/adapters/UserCursorAdapter.java
@@ -0,0 +1,10 @@
+package org.techconnect.adapters;
+
+import android.support.v4.widget.CursorAdapter;
+
+/**
+ * Created by doranwalsten on 11/12/16.
+ */
+
+public class UserCursorAdapter extends CursorAdapter {
+}
---
+++ app/src/main/java/org/techconnect/views/DeviceUserTabbedView.java
@@ -0,0 +1,10 @@
+package org.techconnect.views;
+
+import View;
+
+/**
+ * Created by doranwalsten on 11/12/16.
+ */
+
+public class DeviceUserTabbedView extends View {
+}
